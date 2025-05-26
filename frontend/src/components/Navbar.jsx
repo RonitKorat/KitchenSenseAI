35603cd193c245f5f1fbb7803a0219a01ddd6698
RonitKorat <ronitpatel505@gmail.com>
2025-05-26 14:36:54 +0530

    signup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,23 +8,6 @@ import {
   MoonIcon,
 } from "@heroicons/react/24/outline";
 export default function Navbar() {
-  const [isDark, setisDark] = useState(
-    localStorage.getItem("darkMode") === "true"
-  );
-
-  useEffect(() => {
-    if (isDark) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("darkMode", "true");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("darkMode", "false");
-    }
-  }, [isDark]);
-
-  const toggleDarkMode = () => {
-    setisDark(!isDark);
-  };
   return (
     <>
       <motion.div className="flex items-center text-white justify-between  dark:from-gray-900 dark:to-gray-800 transition-colors duration-300">
@@ -32,34 +15,24 @@ export default function Navbar() {
         <div className="ml-10 text-3xl mt-10 dark:text-purple-400 dark:text-purple-700">
           <Link to="/home">SmartKitchen</Link>
         </div>
-        <div className="mr-20 text-1.5xl mt-10">
-          <ul className="flex gap-8">
-            <li className="hover:text-purple-400">
+        <div className="mr-20 text-1.5xl">
+          <ul className="flex gap- mt-10">
+            <li className="hover:text-purple-400 px-4 py-2">
               <Link to="/">Home</Link>
             </li>
-            <li className="hover:text-purple-400">
+            <li className="hover:text-purple-400 px-4 py-2">
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li className="hover:text-purple-400 px-4 py-2">
               <Link to="/features">Features</Link>
             </li>
-            <li className="hover:text-purple-400">
+            <li className="hover:text-purple-400 px-4 py-2">
               <Link to="/login">Login</Link>
             </li>
-            <li className="hover:text-purple-400">
+            <li className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-md text-sm font-medium transition-colors duration-200">
               <Link to="/signup">SignUp</Link>
             </li>
-            <li>
-              <button onClick={toggleDarkMode} id="dark">
-                {isDark ? (
-                  <motion.div whileHover={{ rotate: 10 }}>
-                    <SunIcon className="h-5 w-5 text-gray-300" />
-                  </motion.div>
-                ) : (
-                  <MoonIcon
-                    className="h-5 w-5 text-dark-300"
-                    whileHover={{ rotate: 10 }}
-                  />
-                )}
-              </button>
-            </li>
+            <li></li>
           </ul>
         </div>
         {/* </div> */}
