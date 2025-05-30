5be0cd76c706d0b5f4bd063b2cd918d33f1818b3
RonitKorat <ronitpatel505@gmail.com>
2025-05-30 21:18:31 +0530

    image preview below choose file done

--- frontend/src/pages/Inventory.jsx
+++ frontend/src/pages/Inventory.jsx
@@ -115,137 +115,183 @@ const Inventory = () => {
 
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ opacity: 0.8, duration: 0.5 }}
-        className="text-center bg-gradient-to-br from-green-50 to-green-100 dark:from-gray-900 dark:to-gray-800 transition-colors duration-300"
-      >
-        <h1 className="pt-5 text-4xl font-bold text-gray-800 dark:text-white mb-4">
-          AI-Powered Inventory Management
-        </h1>
-        <p className="text-xl text-gray-600 dark:text-gray-300">
-          Track and manage your inventory with real-time AI detection
-        </p>
-
+      <div className="text-center bg-gradient-to-br from-green-50 to-green-100 dark:from-gray-900 dark:to-gray-800 transition-colors duration-300">
         <motion.div
-          className="mx-25 mt-5 text-white text-center bg-gradient-to-br from-green-50 to-green-100 dark:from-gray-800 dark:to-gray-700 transition-colors duration-300 p-6 rounded-xl shadow-lg"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1.5 }}
+          transition={{ opacity: 0.8, duration: 0.5 }}
+          className="text-center bg-gradient-to-br from-green-50 to-green-100 dark:from-gray-900 dark:to-gray-800 transition-colors duration-300"
         >
-          <div>
-            <h2 className="text-2xl font-semibold">
-              Real Time Stock Detection
-            </h2>
+          <h1 className="pt-5 text-4xl font-bold text-gray-800 dark:text-white mb-4">
+            AI-Powered Inventory Management
+          </h1>
+          <p className="text-xl text-gray-600 dark:text-gray-300">
+            Track and manage your inventory with real-time AI detection
+          </p>
 
-            <div className="bg-gray-600 transition-colors duration-300 mt-5 mx-auto rounded-xl w-[400px] h-[250px] overflow-hidden flex items-center justify-center ">
-              {isCameraOn && (
-                <Webcam
-                  audio={false}
-                  ref={webcamRef}
-                  screenshotFormat="image/png"
-                  videoConstraints={videoConstraints}
-                  width={400}
-                  height={250}
-                  placeholder="capture video"
-                />
+          <motion.div
+            className="mx-25 mt-5 text-white text-center bg-gradient-to-br from-green-50 to-green-100 dark:from-gray-800 dark:to-gray-700 transition-colors duration-300 p-6 rounded-xl shadow-lg"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.5 }}
+          >
+            <div>
+              <h2 className="text-2xl font-semibold">
+                Real Time Stock Detection
+              </h2>
+
+              <div className="bg-gray-600 transition-colors duration-300 mt-5 mx-auto rounded-xl w-[400px] h-[250px] overflow-hidden flex items-center justify-center ">
+                {isCameraOn && (
+                  <Webcam
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/png"
+                    videoConstraints={videoConstraints}
+                    width={400}
+                    height={250}
+                    placeholder="capture video"
+                  />
+                )}
+              </div>
+
+              <div className="flex mt-5 justify-center gap-4 flex-wrap">
+                <button
+                  className={`px-4 py-2 border-2 border-white rounded-xl bg-green-500 text-white-700 hover:bg-green-600 hover:text-white transition duration-300 disabled:opacity-50 ${
+                    isCameraOn
+                      ? "bg-gray-400 cursor-not-allowed"
+                      : "bg-green-600 hover:bg-green-700 cursor-cell"
+                  }`}
+                  onClick={startCamera}
+                  disabled={isCameraOn}
+                >
+                  Start Camera
+                </button>
+                <button
+                  className={`px-4 py-2 border-2 border-white rounded-xl  text-white  transition duration-300 disabled:opacity-50 ${
+                    !isCameraOn
+                      ? "bg-gray-400 cursor-not-allowed"
+                      : "bg-red-600 hover:bg-red-700 cursor-cell"
+                  }`}
+                  onClick={stopCamera}
+                  disabled={!isCameraOn}
+                >
+                  Stop Camera
+                </button>
+                <button
+                  className={`px-4 py-2 border-2 border-white rounded-xl  text-white hover:text-white transition duration-300 disabled:opacity-50  ${
+                    !isCameraOn
+                      ? "bg-gray-400 cursor-not-allowed"
+                      : "bg-purple-800 hover:bg-pueple-700 cursor-cell"
+                  }`}
+                  onClick={captureImage}
+                  disabled={!isCameraOn}
+                >
+                  Capture Image
+                </button>
+              </div>
+
+              {capturedImage && (
+                <div className="mt-6 text-center">
+                  <h3 className="text-lg font-medium mb-2">Captured Image:</h3>
+                  <img
+                    src={capturedImage}
+                    alt="Captured"
+                    className="w-80 border-2 border-gray-300 rounded-md mx-auto"
+                  />
+                </div>
               )}
             </div>
-
-            <div className="flex mt-5 justify-center gap-4 flex-wrap">
-              <button
-                className={`px-4 py-2 border-2 border-white rounded-xl bg-green-500 text-white-700 hover:bg-green-600 hover:text-white transition duration-300 disabled:opacity-50 ${
-                  isCameraOn
-                    ? "bg-gray-400 cursor-not-allowed"
-                    : "bg-green-600 hover:bg-green-700 cursor-cell"
+          </motion.div>
+          <motion.div className="mt-20">
+            <div>
+              <div
+                className={`w-250 h-50 mx-auto border-2 border-dashed rounded-lg p-8 text-center ${
+                  isDragging
+                    ? "border-green-500 bg-green-50 dark:bg-green-900/20"
+                    : "border-gray-300 dark:border-gray-600"
                 }`}
-                onClick={startCamera}
-                disabled={isCameraOn}
+                onDragEnter={handleDragEnter}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleOnDrop}
               >
-                Start Camera
-              </button>
-              <button
-                className={`px-4 py-2 border-2 border-white rounded-xl  text-white  transition duration-300 disabled:opacity-50 ${
-                  !isCameraOn
-                    ? "bg-gray-400 cursor-not-allowed"
-                    : "bg-red-600 hover:bg-red-700 cursor-cell"
-                }`}
-                onClick={stopCamera}
-                disabled={!isCameraOn}
-              >
-                Stop Camera
-              </button>
-              <button
-                className={`px-4 py-2 border-2 border-white rounded-xl  text-white hover:text-white transition duration-300 disabled:opacity-50  ${
-                  !isCameraOn
-                    ? "bg-gray-400 cursor-not-allowed"
-                    : "bg-purple-800 hover:bg-pueple-700 cursor-cell"
-                }`}
-                onClick={captureImage}
-                disabled={!isCameraOn}
-              >
-                Capture Image
-              </button>
-            </div>
-
-            {capturedImage && (
-              <div className="mt-6 text-center">
-                <h3 className="text-lg font-medium mb-2">Captured Image:</h3>
-                <img
-                  src={capturedImage}
-                  alt="Captured"
-                  className="w-80 border-2 border-gray-300 rounded-md mx-auto"
-                />
-              </div>
-            )}
-          </div>
-        </motion.div>
-        <motion.div className="mt-20">
-          <div>
-            <div
-              className={`w-250 h-50 mx-auto border-2 border-dashed rounded-lg p-8 text-center ${
-                isDragging
-                  ? "border-green-500 bg-green-50 dark:bg-green-900/20"
-                  : "border-gray-300 dark:border-gray-600"
-              }`}
-              onDragEnter={handleDragEnter}
-              onDragOver={handleDragOver}
-              onDragLeave={handleDragLeave}
-              onDrop={handleOnDrop}
-            >
-              <div className="flex space-x-4 justify-center mt-15 ">
-                <label
-                  htmlFor="file-upload"
-                  className="relative cursor-pointer bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700 transition-all duration-200 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-                >
-                  <span>Choose Files</span>
-                  <input
-                    id="file-upload"
-                    name="file-upload"
-                    type="file"
-                    // ref={fileInputRef}
-                    className="hidden"
-                    accept="image/*"
-                    multiple
-                    onChange={handleFileInput}
-                  />
-                </label>
-                <div className="text-sm text-gray-500 dark:text-gray-400">
-                  Supports: PNG, JPG, GIF
+                <div className="flex space-x-4 justify-center mt-15 ">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700 transition-all duration-200 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+                  >
+                    <span>Choose Files</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      // ref={fileInputRef}
+                      className="hidden"
+                      accept="image/*"
+                      multiple
+                      onChange={handleFileInput}
+                    />
+                  </label>
+                  <div className="text-sm text-gray-500 dark:text-gray-400">
+                    Supports: PNG, JPG, GIF
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <button
-            className="mt-10 px-6 py-2 bg-green-600 hover:bg-green-700 rounded-lg font-bold text-white"
-            onClick={uploadImages}
-            type="button"
-          >
-            Submit
-          </button>
+          </motion.div>
+          {images.length === 0 && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              className="text-center text-gray-500 dark:text-gray-400 mt-8 w-250 h-50 mx-auto"
+            >
+              <div className="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-8">
+                <svg
+                  className="mx-auto h-12 w-12 text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+                <h3 className="mt-2 text-sm font-medium text-gray-900 dark:text-white">
+                  No images
+                </h3>
+                <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
+                  Get started by uploading your inventory images.
+                </p>
+              </div>
+            </motion.div>
+          )}
+          {images.length > 0 && (
+            <div className="mt-10">
+              <div className="ml-30 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 mt-5 mr-30">
+                {images.map((image, index) => (
+                  <div key={index}>
+                    <img
+                      src={image.preview}
+                      alt={`Uploaded image ${index + 1}`}
+                      className="w-full h-50 object-cover border rounded-2xl "
+                    />
+                  </div>
+                ))}
+              </div>
+              <button
+                className="mt-10 px-6 py-2 bg-green-600 hover:bg-green-700 rounded-lg font-bold text-white"
+                onClick={uploadImages}
+                type="button"
+              >
+                Process
+              </button>
+            </div>
+          )}
         </motion.div>
-      </motion.div>
+      </div>
     </>
   );
 };
